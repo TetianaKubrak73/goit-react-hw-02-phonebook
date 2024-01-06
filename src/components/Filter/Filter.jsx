5f9b903d2dd11561938ee1ca8d1316256a3941eb
TetianaKubrak73 <tetianakubrak73@gmail.com>
2024-01-07 00:02:41 +0100

    add components

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+import style from './Filter.module.css';
+function Filter({ value, onChangeFilter }) {
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input type="text" value={value} onChange={onChangeFilter} />
+      </label>
+    </div>
+  );
+}
+
+export default Filter;
